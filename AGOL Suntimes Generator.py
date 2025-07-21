108dbf498a763acbfba3e2b52e4e1410d1c6d937
ColbymSpencer <70975330+ColbymSpencer@users.noreply.github.com>
2025-07-21 14:28:45 -0400

    Add files via upload

--- AGOL Suntimes Generator.py
+++ AGOL Suntimes Generator.py
@@ -5,40 +5,52 @@ from datetime import date, datetime, timedelta
 from zoneinfo import ZoneInfo  # Only available in Python 3.9+
 import pandas as pd
 
+def generate_centroids(host_gdb = r"\\floridadep\data\DRP\GIS\Codes and Scripts\Python3x\Sunset Sunrise Calculator\Sunrise Sunset Testing2\Sunrise Sunset Popups.gdb",
+                        park_bounds = 'ParkBounds',
+                        timezones = 'Timezones'):
+    """
+    From park boundaries and timezones feature claasses, create a dictionary with the
+    centroids of each park in decimal degrees and the time zone the
+    centroid intersects.
 
-### GENERATE CENTROID TABLE ####
-## table with the lat and long of the centroid of each park, the parks name, and the timezone it is in.
+    Inputs:
+        host_gdb: file path for a geodatabase containing:
+            - park boundaries polyon featureclass
+            - timezones polygon feature class
+        park_bounds: name of the park boundary feature class
+        timezones: name of the timezones feature class
+    Returns:
+        dictionary of the lat, long, and timezone for each park in the format
+            park:[(lat,lon),timezone]
+            Timezone is the IANA time zone string (e.g., "America/New_York")
+    """
+    # Worspace and settings
+    arcpy.env.workspace = host_gdb
+    arcpy.env.overwriteOutput = True
 
-# Worspace and settings
-arcpy.env.workspace = r'C:\Users\Spencer_C\Documents\ArcGIS\Projects\Sunrise Sunset Popups\Sunrise Sunset Popups.gdb'
-arcpy.env.overwriteOutput = True
+    # Calculate lat, long coordinates
+    arcpy.management.CalculateGeometryAttributes(park_bounds, [["lon", "CENTROID_X"], ["lat", "CENTROID_Y"]], coordinate_format = "DD")
 
-# Calculate lat, long coordinates
-arcpy.management.CalculateGeometryAttributes("ParkBounds", [["lon", "CENTROID_X"], ["lat", "CENTROID_Y"]], coordinate_format = "DD")
+    # Join timezones to park boundaries
+    arcpy.analysis.SpatialJoin(target_features = park_bounds,
+                                join_features = timezones,
+                                out_feature_class = 'PB_Timezones')
 
-# Join timezones to park boundaries
-arcpy.analysis.SpatialJoin(target_features = r'C:\Users\Spencer_C\Documents\ArcGIS\Projects\Sunrise Sunset Popups\Sunrise Sunset Popups.gdb\ParkBounds',
-                            join_features = r'C:\Users\Spencer_C\Documents\ArcGIS\Projects\Sunrise Sunset Popups\Sunrise Sunset Popups.gdb\Timezones',
-                            out_feature_class = r'C:\Users\Spencer_C\Documents\ArcGIS\Projects\Sunrise Sunset Popups\Sunrise Sunset Popups.gdb\PB_Timezones')
+    # Create dicitonary for output
+    centroid_dict = {}
 
-# Create dicitonary for output
-centroid_dict = {}
+    # Fill dictionary
+    with arcpy.da.SearchCursor("PB_Timezones", ['SITE_NAME', 'lon','lat','IANA_tz']) as cursor:
+        for row in cursor:
+            site_name = row[0]
+            lon = row[1]
+            lat = row[2]
+            timezone = row[3]
 
-# Fill dictionary
-with arcpy.da.SearchCursor("PB_Timezones", ['SITE_NAME', 'lon','lat','IANA_tz']) as cursor:
-    for row in cursor:
-        site_name = row[0]
-        lon = row[1]
-        lat = row[2]
-        timezone = row[3]
+            centroid_dict[site_name] = [(lat,lon),timezone]
+    print('Centroids generated')
+    return centroid_dict
 
-        centroid_dict[site_name] = [(lat,lon),timezone]
-
-#print(centroid_dict)
-
-
-#### CALCULATE SUNSET AND SUNRISE TIMES FOR EACH PARK #####
-#function to calculate sunrise and sunset times
 def calculate_sunrise_sunset(lat, lon, year, month, day, timezone_str):
     """
     Calculate sunrise and sunset times in local time zone.
@@ -110,57 +122,81 @@ def calculate_sunrise_sunset(lat, lon, year, month, day, timezone_str):
 
     return sunrise_str, sunset_str
 
-# Get date list
-today = date.today()
-dates = [today + timedelta(days=i) for i in range(190)]
+def generate_suntable(centorid_dict, date_range = 10):
+    """
+    Calculate sunrise and sunset times in local time zone.
 
-# List to hold data in
-records = []
+    Inputs:
+        centroid_dict (dict):
+            in the format park:(lat,lon),timezone]
+        date_range (int):
+            Number of days from today to calculate the sunset and sunrise for
+            less than or equal to 100 due to timeout issues when writing data
+            to AGO
+    Returns:
+        sun_df (pandas dataframe): dataframe containing a record of sunset and sunrise times for every park in
+        the input dictionary for every date from now until {date_range} days in the future
+    """
+    # Get date list
+    today = date.today()
+    dates = [today + timedelta(days=i) for i in range(date_range)]
 
-# Loop through each item in centroid dictionary
-for key, val in centroid_dict.items():
-    lat,lon = val[0]
-    tz = val[1]
-    park = key
+    # List to hold data in
+    records = []
 
-    for d in dates:
-        sunrise, sunset = calculate_sunrise_sunset(lat, lon, d.year, d.month, d.day, tz)
-        records.append({
-            'park': park,
-            'timezone': tz,
-            'date': f'date is |{d}',
-            'sunrise': f'sunrise is |{sunrise}',
-            'sunset': f'sunset is |{sunset}'
-        })
+    # Loop through each item in centroid dictionary
+    for key, val in centroid_dict.items():
+        lat,lon = val[0]
+        tz = val[1]
+        park = key
 
-# Create table
-sun_df = pd.DataFrame(records)
-print('Suntable successfully generated')
+        for d in dates:
+            sunrise, sunset = calculate_sunrise_sunset(lat, lon, d.year, d.month, d.day, tz)
+            records.append({
+                'park': park,
+                'timezone': tz,
+                'date': f'{d}',
+                'sunrise': f'{sunrise}',
+                'sunset': f'{sunset}'
+            })
+
+    # Create table
+    sun_df = pd.DataFrame(records)
+    print('Suntable generated')
+    return sun_df
 
 #### USE TABLE TO OVERWRITE CURRENT DATA####
 
-# Import packages
-from arcgis.gis import GIS
+def update_AGO_table(replacement_table, item_id = "c1bd4c99f7904eb9b629f0bb55e170eb"):
+    # Import packages
+    from arcgis.gis import GIS
 
-# Sign in to ArcGIS Online account
-gis = GIS(
-        url="https://fdep.maps.arcgis.com/home/index.html",
-        username="",
-        password=""
-    )
+    # Sign in to ArcGIS Online account
+    gis = GIS(
+            url="https://fdep.maps.arcgis.com/home/index.html",
+            username="FLPARKSGIS",
+            password="FLPARKSGIS1"
+        )
 
-#Get suntable from AGO
-suntable_item = gis.content.get("c1bd4c99f7904eb9b629f0bb55e170eb") # item id ('ServiceItemId') for the related table. Found in JSON for the table on AGO
-suntable_ago = suntable_item.tables[0]
+    # Get suntable from AGO
+    suntable_item = gis.content.get(item_id) # item id ('ServiceItemId') for the related table. Found in JSON for the table on AGO
+    suntable_ago = suntable_item.tables[0]
 
-# Delete all the data in the suntable
-try:
-    suntable_ago.manager.truncate()
-except:
-    print('unable to truncate')
+    # Delete data in the suntable
+    try:
+        suntable_ago.manager.truncate()
+    except Exception as e:
+        print(f'Unable to truncate the related table of sunset and sunrise times: {e}')
 
-# Replace all the data in the suntable
-try:
-    suntable_ago.edit_features(adds=sun_df.spatial.to_featureset())
-except:
-    print('Unbable to add new data to the related table')
+    # Replace all the data in the suntable
+    try:
+        suntable_ago.edit_features(adds=sun_df.spatial.to_featureset())
+        print('Related table updated successfully')
+    except Exception as e:
+        print(f'Unbable to add new data to the related table: {e}')
+
+#local_copy_gdb = r"C:\Users\Spencer_C\Documents\ArcGIS\Projects\Sunrise Sunset Popups\Sunrise Sunset Popups.gdb"
+
+centroid_dict = generate_centroids()
+sun_df = generate_suntable(centroid_dict, date_range = 100)
+update_AGO_table(replacement_table=sun_df)
